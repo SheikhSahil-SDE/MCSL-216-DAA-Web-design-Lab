07e1806a8b738d0234ff25110c8420257e441dc9
Sheikh Sahil <sheikha.sahil@gmail.com>
2025-05-13 22:04:04 +0530

    Delete Section 2: Web design/Session-05/Session-06 directory

--- Section 2: Web design/Session-05/Session-06/2.js
+++
@@ -1,6 +0,0 @@
-// 2. Which JavaScript function can be used to dynamically add a event listener to a HTML Element?
-
-// The JavaScript function that can be used to dynamically add an event 
-// listener to an HTML element is:
-
-element.addEventListener(event, function, useCapture);
